bcaf8d59259e47022b7eb85b9a57fb895cf6ce8f
鷹取敏志 <takatorix@gmail.com>
2020-02-02 23:38:35 +0900

    update

---
+++ front/src/components/TaskList/TaskList.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import Task from '../Task/Task';
+
+export type TaskListProps = {
+    loading?: Boolean,
+    tasks: Task[],
+    onArchiveTask: Function,
+    onPinTask: Function,
+}
+
+export const TaskList: React.FC<TaskListProps> = ({loading = false, tasks, onArchiveTask, onPinTask}) => {
+    const events = {onPinTask, onArchiveTask};
+    const LoadingRow = (
+        <div className="loading-item">
+            <span className="glow-checkbox"/>
+            <span className="glow-text">
+                <span>Loading</span> <span>cool</span> <span>state</span>
+            </span>
+        </div>
+    );
+
+    if (loading) {
+        return (
+            <div className="list-items">
+                {LoadingRow}
+                {LoadingRow}
+                {LoadingRow}
+                {LoadingRow}
+                {LoadingRow}
+                {LoadingRow}
+            </div>);
+    }
+    if (tasks.length === 0) {
+        return (
+            <div className="list-items">
+                <div className="wrapper-message">
+                    <span className="icon-check"/>
+                    <div className="title-message">You have no tasks</div>
+                    <div className="subtitle-message">Sit back and relax</div>
+                </div>
+            </div>
+        );
+    }
+
+    const tasksInOrder = [
+        ...tasks.filter(t => t.state === 'TASK_PINNED'),
+        ...tasks.filter(t => t.state !== 'TASK_PINNED'),
+    ];
+
+    return (
+        <div className="list-items">
+            {tasksInOrder.map(task => (
+                <Task key={task.id} task={task} {...events} />
+            ))}
+        </div>
+    );
+};
+
+
+export default TaskList
+
+

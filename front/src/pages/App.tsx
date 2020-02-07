b7722abb82f007b08cb703f7077fbdf763289f6a
鷹取敏志 <takatorix@gmail.com>
2020-02-07 23:31:20 +0900

    update

--- front/src/pages/App.tsx
+++ front/src/pages/App.tsx
@@ -1,32 +1,13 @@
 import React from 'react';
-import logo from '../assets/logo.svg';
-import './App.css';
 import {Provider as ReduxProvider} from 'react-redux'
 import {setupStore} from "../reducers";
 
 const store = setupStore();
-
 const App: React.FC = () => {
     return (
         <ReduxProvider store={store}>
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <p>
-                        Edit <code>src/App.tsx</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                </header>
-            </div>
         </ReduxProvider>
     );
-}
+};
 
 export default App;

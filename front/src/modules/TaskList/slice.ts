981b92218f776eb91b462c6233f7f7a497d3acc0
鷹取敏志 <takatorix@gmail.com>
2020-02-09 17:18:20 +0900

    update

---
+++ front/src/modules/TaskList/slice.ts
@@ -0,0 +1,27 @@
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+import Task from "../../components/Task/Task";
+
+export type State = {
+    tasks: Task[]
+}
+const defaultTasks = [
+    {id: '1', title: 'Something', state: 'TASK_INBOX'},
+    {id: '2', title: 'Something more', state: 'TASK_INBOX'},
+    {id: '3', title: 'Something else', state: 'TASK_INBOX'},
+    {id: '4', title: 'Something again', state: 'TASK_INBOX'},
+];
+
+const slice = createSlice({
+    name: "taskList",
+    initialState: {tasks: defaultTasks},
+    reducers: {
+        archiveTask: (state, action: PayloadAction<{ id: string }>) => {
+            state.tasks.map(task =>
+                task.id === action.payload.id ? {...task, state: 'TASK_ARCHIVED'} : task)
+        },
+        pinTask: (state, action: PayloadAction<{ id: string }>) => {
+            state.tasks.map(task =>
+                task.id === action.payload.id ? {...task, state: 'TASK_PINNED'} : task)
+        },
+    }
+});

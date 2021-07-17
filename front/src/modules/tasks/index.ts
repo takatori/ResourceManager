eafea24f129b987bf87acab463d36bf6e9ec27c0
鷹取敏志 <takatorix@gmail.com>
2021-07-17 14:28:21 +0900

    Remove: frontend

--- front/src/modules/tasks/index.ts
+++
@@ -1,6 +0,0 @@
-import {selectors as taskListSelectors} from "./selectors";
-import reducer, {actions as taskListActions} from "./slice";
-
-export {taskListSelectors, taskListActions}
-
-export default reducer;
